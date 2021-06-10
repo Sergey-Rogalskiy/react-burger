bff1d082fa0a72bd96c9db3961bb5ebd668cde01
Sergey Rogalskiy <rogalskiy.sergey@gmail.com>
2021-06-10 19:54:54 +0300

    fix header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,64 +1,36 @@
-import {
-  Logo, 
-  BurgerIcon, 
-  ListIcon,
-  ProfileIcon
-} from '@ya.praktikum/react-developer-burger-ui-components'
-import { Link } from "react-router-dom"
-
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import { NavLink } from "react-router-dom"
 import s from './app-header.module.css'
 
 function AppHeader() {
   return (
-<>
     <header className={s.header}>
-      <div className={s.container}>
-        <div className={s.header__inner}>
+      <div className={s.header__inner}>
+        <nav className={s.nav}>
+            <NavLink className={s.nav__link} to=".">
+              <BurgerIcon type="primary"/>
+              <span>Конструктор</span>
+            </NavLink>
+            <NavLink className={s.nav__link} to="/feed">
+              <ListIcon type="primary" />
+              <span>Лента заказов</span>
+            </NavLink>
+        </nav>
 
-          <div>
-            <nav className={s.nav}>
-            <Link className={s.nav__link} to=".">
-                  <BurgerIcon type="primary"/>
-                  <span>
-                    Конструктор
-                  </span>
-                </Link>
-                <Link className={s.nav__link} to="/feed">
-                  <ListIcon type="primary" />
-                  <span>
-                    Лента заказов
-                  </span>
-                </Link>
-            </nav>
-          </div>
-
-          <div className={s.header__logo}>
-            <Logo />
-          </div>
-
-          <div>
-            <nav className={s.nav}>
-                <Link className={s.nav__link} to="/profile">
-                    <ProfileIcon type="primary" />
-                    <span>
-                      Личный кабинет
-                    </span>
-                </Link>
-            </nav>
-          </div>
-
+        <div className={s.header__logo}>
+          <Logo />
         </div>
-      </div>
-    </header>
 
-    
-    <div>
-      <span>
-      </span>
+          <nav className={s.nav}>
+              <NavLink className={s.nav__link} to="/profile">
+                  <ProfileIcon type="primary" />
+                  <span>
+                    Личный кабинет
+                  </span>
+              </NavLink>
+          </nav>
     </div>
-    
-
-    </>
+    </header>
   );
 }
 
