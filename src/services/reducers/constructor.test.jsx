64d071070a44309680f01136bd7846db60291029
Sergey Rogalskiy <rogalskiy.sergey@gmail.com>
2021-06-20 20:25:38 +0300

    dependicies

---
+++ src/services/reducers/constructor.test.jsx
@@ -0,0 +1,118 @@
+import {
+    GET_ORDER_FAILED,
+    GET_ORDER_REQUEST,
+    GET_ORDER_SUCCESS,
+    ADD_ITEM_TO_CONSTRUCTOR,
+    DELETE_ITEM_FROM_CONSTRUCTOR,
+    CHANGE_ORDER_OF_ITEMS_IN_CONSTRUCTOR,
+    ORDER_RESET,
+  } from '../actions/constructor';
+  
+
+  const initialState = {
+
+    order: null,
+    orderRequest: false,
+    orderFailed: false,
+  
+    chosenItems: [],
+    chosenBuns: {},
+
+    totalPrice: 0,
+    totalPriceBuns: 0
+  };
+  
+  export const constructorReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+      case GET_ORDER_REQUEST: {
+        return {
+          ...state,
+          orderRequest: true
+        };
+      }
+      case GET_ORDER_SUCCESS: {
+        return { 
+          ...state, 
+          orderFailed: false,
+          order: action.order, 
+          orderRequest: false
+        };
+      }
+      case GET_ORDER_FAILED: {
+        return { 
+          ...state, 
+          orderFailed: true, 
+          orderRequest: false 
+        };
+      }
+      case ORDER_RESET: {
+        return { 
+          ...state, 
+          orderFailed: false,
+          order: null, 
+          orderRequest: false
+        };
+      }
+
+      case ADD_ITEM_TO_CONSTRUCTOR: {
+        if (action.item.type === "bun") {
+          let priceItems = 0
+          if (state.chosenItems[0]) {
+            priceItems = state.chosenItems.reduce((a, b) => a + b.price, 0)
+          }
+          const totalPrice = action.item.price * 2 +priceItems
+          return { 
+            ...state, 
+            chosenBuns: action.item,
+            totalPrice
+          };
+        }
+
+        let priceBuns = 0
+        if (state.chosenBuns) {
+          priceBuns = state.chosenBuns.price *2
+        }
+        const newItems = [...state.chosenItems, action.item]
+        const priceItems = newItems.reduce((a, b) => a + b.price, 0)
+        const totalPrice = priceBuns+priceItems
+        return { 
+          ...state, 
+          chosenItems: newItems,
+          totalPrice
+        }
+      }
+      case DELETE_ITEM_FROM_CONSTRUCTOR: {
+        var chosenItems = [...state.chosenItems]
+        chosenItems.splice(action.index, 1)
+        
+        const priceForNewItems = chosenItems.reduce((a, b) => a + b.price, 0)
+        const priceBuns =  state.chosenBuns.price * 2
+        let totalPrice = priceBuns+priceForNewItems
+
+        return { ...state, 
+          chosenItems: chosenItems,
+          totalPrice
+         };
+      }
+      case CHANGE_ORDER_OF_ITEMS_IN_CONSTRUCTOR: {
+        const dragCard = state.chosenItems[action.dragIndex];
+        if (dragCard){
+          const newArray = [
+            ...state.chosenItems,
+          ]
+          newArray.splice(action.dragIndex, 1)
+          newArray.splice(action.hoverIndex, 0, dragCard)
+
+          return { 
+            ...state, 
+            chosenItems: newArray
+          };
+        }
+      }
+      default: {
+        return state;
+      }
+    }
+  };
+  
