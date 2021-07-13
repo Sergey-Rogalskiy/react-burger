9c38e163639e4028f625739e78aa808c8c000f63
Sergey Rogalskiy <rogalskiy.sergey@gmail.com>
2021-07-13 12:46:43 +0300

    typed actions

---
+++ src/services/actions/feed.tsx
@@ -0,0 +1,65 @@
+
+import { 
+  getOrderByIdRequest, 
+} from '../real-service';
+
+export const WS_CONNECTION_START:'WS_CONNECTION_START' = 'WS_CONNECTION_START';
+export const WS_CONNECTION_SUCCESS:'WS_CONNECTION_SUCCESS' = 'WS_CONNECTION_SUCCESS';
+export const WS_CONNECTION_ERROR:'WS_CONNECTION_ERROR' = 'WS_CONNECTION_ERROR';
+export const WS_GET_MESSAGE:'WS_GET_MESSAGE' = 'WS_GET_MESSAGE';
+export const WS_SEND_MESSAGE:'WS_SEND_MESSAGE' = 'WS_SEND_MESSAGE';
+export const WS_CONNECTION_CLOSED:'WS_CONNECTION_CLOSED' = 'WS_CONNECTION_CLOSED';
+
+export const WS_CONNECTION_AUTH_START:'WS_CONNECTION_AUTH_START' = 'WS_CONNECTION_AUTH_START';
+export const WS_CONNECTION_AUTH_SUCCESS:'WS_CONNECTION_AUTH_SUCCESS' = 'WS_CONNECTION_AUTH_SUCCESS';
+export const WS_CONNECTION_AUTH_ERROR:'WS_CONNECTION_AUTH_ERROR' = 'WS_CONNECTION_AUTH_ERROR';
+export const WS_GET_MESSAGE_AUTH:'WS_GET_MESSAGE_AUTH' = 'WS_GET_MESSAGE_AUTH';
+export const WS_SEND_MESSAGE_AUTH:'WS_SEND_MESSAGE_AUTH' = 'WS_SEND_MESSAGE_AUTH';
+export const WS_CONNECTION_AUTH_CLOSED:'WS_CONNECTION_AUTH_CLOSED' = 'WS_CONNECTION_AUTH_CLOSED';
+
+export const ORDER_ID_REQUEST:'ORDER_ID_REQUEST' = 'ORDER_ID_REQUEST';
+export const ORDER_ID_SUCCESS:'ORDER_ID_SUCCESS' = 'ORDER_ID_SUCCESS';
+export const ORDER_ID_FAILED:'ORDER_ID_FAILED' = 'ORDER_ID_FAILED';
+
+
+export function wsInit() {
+    return function(dispatch) {
+      dispatch({
+        type: WS_CONNECTION_START
+      })
+    }
+  }
+export function wsInitAuth() {
+    return function(dispatch) {
+      dispatch({
+        type: WS_CONNECTION_AUTH_START
+      })
+    }
+  }
+  
+
+export function getOrderById(data) {
+    return function(dispatch) {
+      dispatch({
+        type: ORDER_ID_REQUEST
+      });
+      getOrderByIdRequest(data)
+      .then(res => {
+        if (res && res.success) {
+          dispatch({
+            type: ORDER_ID_SUCCESS,
+            payload: res
+          });
+        } else {
+          dispatch({
+            type: ORDER_ID_FAILED
+          });
+        }
+      })
+      .catch(err => {
+          dispatch({
+            type: ORDER_ID_FAILED
+          });
+      })
+    };
+  }
--- src/services/actions/ingridients.tsx
+++ src/services/actions/ingridients.tsx
@@ -2,13 +2,13 @@ import {
     getIngridientsRequest, 
 } from '../real-service';
 
-export const GET_ITEMS_REQUEST = 'GET_ITEMS_REQUEST';
-export const GET_ITEMS_SUCCESS = 'GET_ITEMS_SUCCESS';
-export const GET_ITEMS_FAILED = 'GET_ITEMS_FAILED';
+export const GET_ITEMS_REQUEST:'GET_ITEMS_REQUEST' = 'GET_ITEMS_REQUEST';
+export const GET_ITEMS_SUCCESS:'GET_ITEMS_SUCCESS' = 'GET_ITEMS_SUCCESS';
+export const GET_ITEMS_FAILED:'GET_ITEMS_FAILED' = 'GET_ITEMS_FAILED';
 
-export const TAB_SWITCH = 'TAB_SWITCH';
+export const TAB_SWITCH:'TAB_SWITCH' = 'TAB_SWITCH';
 
-export const SET_CURRENT_ITEM_TO_VIEW = 'SET_CURRENT_ITEM_TO_VIEW';
+export const SET_CURRENT_ITEM_TO_VIEW:'SET_CURRENT_ITEM_TO_VIEW' = 'SET_CURRENT_ITEM_TO_VIEW';
 
 export function setCurrentItemToView(currentItemToView) {
   return function(dispatch) {
